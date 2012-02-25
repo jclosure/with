175e5f62c772c5e157d4c8cfdb1932b69bad9dea
jclosure <jclosure@gmail.com>
2012-02-25 15:29:35 -0600

    initial creation of otb rails site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+With::Application.config.secret_token = '378c375cf083e25795c823470cf32591518563ddae27319c3aa618b8a981ed0402d770c46d11ec6328de953a929b6b427abc29cbbfc55f81b3dfe95a5b87d1c9'
