88d0984cb26fdacb15ca1d88d5b881a214319485
jclosure <jclosure@gmail.com>
2012-10-31 23:22:01 -0500

    z-axis of rc frame set to 9999.  on top in test cases now

--- public/javascripts/uberRC.js
+++ public/javascripts/uberRC.js
@@ -10,12 +10,11 @@ var uberRemoteControl = function(ui_url, system){
 				<div id='rcframe_veil'>\
 					<p>Loading...</p>\
 				</div>\
-				<iframe src='"+document.home+ui_url+"?source="+encodeURIComponent(document.location)+"#"+encodeURIComponent(document.location.protocol+"//"+document.location.host)+"' onload=\"window.rc.wireCommands()\">Enable iFrames.</iframe>\
+				<iframe style='z-index:9999!important;' src='"+document.home+ui_url+"?source="+encodeURIComponent(document.location)+"#"+encodeURIComponent(document.location.protocol+"//"+document.location.host)+"' onload=\"window.rc.wireCommands()\">Enable iFrames.</iframe>\
 				<style type='text/css'>\
-					#rcframe { z-index:10000!important; }\
 					#rcframe_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }\
 					#rcframe_veil p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }\
-					#rcframe iframe { display: none; position: fixed; top: 0px; right: 0px; width: 100%; height: 50px; z-index: 999; border-bottom: 10px solid rgba(0,0,0,.5); margin: -5px 0 0 -5px; }\
+					#rcframe iframe { display: none; position: fixed; top: 0px; right: 0px; width: 100%; height: 50px; border-bottom: 10px solid rgba(0,0,0,.5); margin: -5px 0 0 -5px; }\
 				</style>\
 			</div>";
 	
