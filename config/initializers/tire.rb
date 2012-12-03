d48db0840eb8bb14f1b52ef3d69c5546ae1b3c5f
jclosure <jclosure@gmail.com>
2012-12-03 02:14:20 -0600

    getting ready for deployment to heroku

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -1,14 +1,13 @@
 #Tire::Configuration.url "http://c7ji5kc:jvzsl54de3fqnu8v@jasmine-1709588.us-east-1.bonsai.io"
 #Tire::Configuration.url "http://localhost:9200"
 
-#ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
+ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
 
-if ENV['BONSAI_INDEX_URL']
+if ENV['BONSAI_URL']
   Tire.configure do
-    url "http://c7ji5kc:jvzsl54de3fqnu8v@jasmine-1709588.us-east-1.bonsai.io"
+    url "http://index.bonsai.io/"
   end
-  ENV['BONSAI_INDEX_NAME'] = ENV['BONSAI_INDEX_URL'][/[^\/]+$/]
-else
-  app_name = Rails.application.class.parent_name.underscore.dasherize
-  ENV['BONSAI_INDEX_NAME'] = "#{app_name}-#{Rails.env}"
 end
+
+app_name = Rails.application.class.parent_name.underscore.dasherize
+ENV['BONSAI_INDEX_NAME'] = "#{app_name}-#{Rails.env}"
